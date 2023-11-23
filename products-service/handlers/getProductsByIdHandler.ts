81afd8798a0cb00fb24688607659af9875ed7778
Kate <ekaterina21051988@yandex.by>
2023-11-23 13:01:30 +0200

    fix: logic hor handler has been updated

--- products-service/handlers/getProductsByIdHandler.ts
+++ products-service/handlers/getProductsByIdHandler.ts
@@ -1,16 +1,27 @@
-import { products} from "../products";
-import { createResponse} from "../utils/utils";
+import { products } from "../products";
+import { createResponse } from "../utils/utils";
 
 export const getProductById = (productId: string) => {
-    return products.find(product => product.id === productId);
+    return products.find((product) => product.id === productId);
 };
 
 export const handler = async (event: any) => {
     try {
-        console.log('HELLO', event)
-        return createResponse(200, getProductById("7567ec4b-b10c-48c5-9345-fc73c48a80a1"))}
-    catch (err) {
+        const productId = event.pathParameters.productId;
+        
+        if (!productId) {
+            return createResponse(400, { message: "Product ID is required" });
+        }
+        
+        const product = getProductById(productId);
+        
+        if (!product) {
+            return createResponse(404, { message: "Product not found" });
+        }
+        
+        return createResponse(200, product);
+    } catch (err) {
         // @ts-ignore
-        return createResponse(500, {message: err.message,})
+        return createResponse(500, { message: err.message });
     }
-}
+};
