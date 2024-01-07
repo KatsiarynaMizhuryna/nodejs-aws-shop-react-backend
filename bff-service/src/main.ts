30ee8cbd94ea33590c38d5b8b74d1ffa9e928c7e
Kate <ekaterina21051988@yandex.by>
2024-01-07 16:51:37 +0200

    feat: added deploy command

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
