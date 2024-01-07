c36d92bc0804833f2ebdd760b13b888c918acd3d
Kate <ekaterina21051988@yandex.by>
2024-01-07 12:19:06 +0200

    feat: bff-service has been created

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -6,11 +6,6 @@ import axios from 'axios';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
   
   @Get([ '', 'ping' ])
   healthCheck(): any {
@@ -23,22 +18,20 @@ export class AppController {
   @All('*')
   async handleRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
     const recipientServiceName = req.params[0];
-    const recipientURL = process.env[recipientServiceName];
-    
-    if (!recipientURL) {
-      res.status(400).json({ error: `Recipient service ${recipientServiceName} not found` });
-      return;
-    }
-    
     const method = req.method.toLowerCase();
-    const url = `${recipientURL}${req.url}`;
+    const url = req.url;
     
     try {
-      const response = await axios({ method, url, data: req.body, params: req.query });
+      const response = await this.appService.forwardRequest(
+          recipientServiceName,
+          method,
+          url,
+          req.body,
+          req.query
+      );
       res.status(response.status).json(response.data);
     } catch (error) {
-      const status = error.response ? error.response.status : 500;
-      res.status(status).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   }
 }
--- bff-service/src/app.service.ts
+++ bff-service/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async forwardRequest(recipientServiceName: string, method: string, url: string, body: any, query: any) {
+    const recipientURL = process.env[recipientServiceName];
+    
+    if (!recipientURL) {
+      throw new Error(`Recipient service ${recipientServiceName} not found`);
+    }
+    
+    try {
+      const response = await axios({ method, url: `${recipientURL}${url}`, data: body, params: query });
+      return { status: response.status, data: response.data };
+    } catch (error) {
+      const status = error.response ? error.response.status : 500;
+      throw new Error(error.message);
+    }
   }
 }
+
