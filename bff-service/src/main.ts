6a2b09664abd3a0e7fa96975ceda0a3ce160fb1c
Kate <ekaterina21051988@yandex.by>
2024-01-07 16:18:03 +0200

    feat: added webpack

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import helmet from 'helmet';
 
+const port = process.env.PORT || 8000;
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000,'0.0.0.0');
+  app.enableCors({
+                   origin: (req, callback) => callback(null, true),
+                 });
+  app.use(helmet());
+  
+  await app.listen(port,'0.0.0.0');
 }
 bootstrap();
